d8bccda6a2d9f0a8d7def81d83464158a0a8c1af
Izan de Geer <izandegeer.ua@gmail.com>
2025-09-25 16:27:20 +0200

    Update Actividad1_25_9.java
    Añadidos comments

--- Unidad1/Ejercicios_Izan/src/main/java/org/example/Actividad1_25_9.java
+++ Unidad1/Ejercicios_Izan/src/main/java/org/example/Actividad1_25_9.java
@@ -7,19 +7,19 @@ public class Actividad1_25_9 {
         Scanner teclado = new Scanner(System.in); // Creación de un objeto scanner
 
         System.out.println("> ¿Cómo te llamas?");
-        String nombre = teclado.next();
+        String nombre = teclado.next(); // Guardamos el nombre
 
         System.out.println("> ¿A qué curso vas?");
-        int curso = teclado.nextInt();
+        int curso = teclado.nextInt(); // Guardamos el curso
 
         System.out.println("Hola " + nombre + " bienvenido a " + curso + "ªDAW");
         System.out.println("> Introduce un número: ");
-        int num1 = teclado.nextInt();
+        int num1 = teclado.nextInt(); // Guardamos el 1º numero
 
         System.out.println("> Introduce otro número: ");
-        int num2 = teclado.nextInt();
+        int num2 = teclado.nextInt(); // Guardamos el 2º numero
 
-        int suma = num1 + num2;
-        System.out.println("El resultado de sumar los dos números es: " + suma);
+        int suma = num1 + num2; // Calculamos la suma
+        System.out.println("El resultado de sumar los dos números es: " + suma); 
     }
 }
