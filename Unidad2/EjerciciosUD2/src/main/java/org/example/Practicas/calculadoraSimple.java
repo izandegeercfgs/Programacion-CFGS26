aca78d50a9738947da7766919f2536ea8b638013
Izan de Geer <izandegeer.ua@gmail.com>
2025-10-21 20:59:25 +0200

    ++

--- Unidad2/EjerciciosUD2/src/main/java/org/example/Practicas/calculadoraSimple.java
+++ Unidad2/EjerciciosUD2/src/main/java/org/example/Practicas/calculadoraSimple.java
@@ -8,11 +8,11 @@ public class calculadoraSimple {
 
         System.out.println("**** BIENVENIDO A LA CALCULADORA RÁPIDA ****");
 
+        System.out.println("> Introduce operando: ");
         try {
-            System.out.println("> Introduce operando: ");
-            int operando = teclado.nextInt();
+            int operando = teclado.nextInt(); // Se pide el operando al usuario
 
-            System.out.println("------------------");
+            System.out.println("------------------"); // Despliegue de posibles operaciones
             System.out.println("(+) -> Suma");
             System.out.println("(-) -> Resta");
             System.out.println("(x) -> Multiplicación");
@@ -21,32 +21,32 @@ public class calculadoraSimple {
             System.out.println("------------------");
             System.out.println("Elige una operación:");
 
-            String operacion = teclado.next();
+            String operacion = teclado.next(); // Se pregunta al usuario qué operación quiere realizar junto al operando 1
 
-            if (operacion.equals("+") || operacion.equals("-") || operacion.equals("x") || operacion.equals("/") || operacion.equals("R")) {
-                if (operacion.equals("R")) {
+            if (operacion.equals("+") || operacion.equals("-") || operacion.equals("x") || operacion.equals("/") || operacion.equals("R")) { // Comprueba que la operación introducida sea una de las disponibles
+                if (operacion.equals("R")) { // Raíz cuadrada
                     if (operando > 0) {
                         int resultado = (int) Math.sqrt(operando);
                         System.out.println("Raiz cuadrada de " + operando + " es " + resultado);
                     } else {
                         System.err.println("ERROR. No es posible realizar la Raíz cuadrada de un número negativo.");
                     }
-                } else {
+                } else { // En caso de no ser raíz cuadrada...
                     System.out.println("> Introduce el segundo operando: ");
-                    int operando2 = teclado.nextInt();
+                    int operando2 = teclado.nextInt(); // Pide el segundo operando
 
                     switch (operacion) {
-                        case "+":
+                        case "+": // En caso de suma
                             int suma = operando + operando2;
                             System.out.println("El resultado de " + operando + " + " + operando2 + " es " + suma);
                             break;
 
-                        case "-":
+                        case "-": // En caso de resta
                             int resta = operando - operando2;
                             System.out.println("El resultado de " + operando + " - " + operando2 + " es " + resta);
                             break;
 
-                        case "x":
+                        case "x": // En caso de multiplicación
                             if (operando2 == 0) {
                                 System.out.println("El resultado de " + operando + " x " + operando2 + " es 0");
                             } else {
@@ -54,12 +54,12 @@ public class calculadoraSimple {
                                 System.out.println("El resultado de " + operando + " x " + operando2 + " es " + multiplicacion);
                             }
                             break;
-                        case "/":
-                            if (operando2 == 0 || operando == 0) {
+                        case "/": // En caso de división
+                            if (operando2 == 0 || operando == 0) { // En caso de que uno de los operandos sean 0...
                                 System.err.println("ERROR. No es posible realizar la división de 0, resultado indefinido.");
                                 break;
                             } else {
-                                double division = (double) operando / operando2;
+                                double division = operando / operando2;
                                 System.out.println("El resultado de " + operando + " / " + operando2 + " es " + division);
                                 break;
                             }
@@ -68,9 +68,9 @@ public class calculadoraSimple {
                     }
                 }
             } else {
-                System.err.println("ERROR. Introduce uno de los signos permitidos.");
+                System.err.println("ERROR. Introduce uno de los signos permitidos."); // En caso de no introducir uno de los signos, salta este error
             }
-        } catch (Exception e) {
+        } catch (Exception e) { // Si salta excepción...
             System.err.println("Se ha producido un error. El sistema ha detectado una excepción.");
             System.out.println("Saliendo del programa...");
         }
