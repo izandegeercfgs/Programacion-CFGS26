c0993b734adf375c92368122c58192ee807b2956
Izan de Geer <izandegeer.ua@gmail.com>
2025-11-27 13:04:22 +0100

    ++

--- Unidad3/EjerciciosUD3/src/main/java/org/example/Ejercicios/NotitaAmorGPT.java
+++ Unidad3/EjerciciosUD3/src/main/java/org/example/Ejercicios/NotitaAmorGPT.java
@@ -30,13 +30,26 @@ public class NotitaAmorGPT {
 
             // 1. Determinar movimientos válidos
             switch (posicionAlumno) {
-                case 0: movimientosValidos = "N,E"; break;
-                case 1: movimientosValidos = "N,E,O"; break;
-                case 2: movimientosValidos = "N,O"; break;
-                case 3: movimientosValidos = "S,E"; break;
-                case 4: movimientosValidos = "S,O,E"; break;
-                case 5: movimientosValidos = "S,O"; break;
-                default: movimientosValidos = "";
+                case 0:
+                    movimientosValidos = "N,E";
+                    break;
+                case 1:
+                    movimientosValidos = "N,E,O";
+                    break;
+                case 2:
+                    movimientosValidos = "N,O";
+                    break;
+                case 3:
+                    movimientosValidos = "S,E";
+                    break;
+                case 4:
+                    movimientosValidos = "S,O,E";
+                    break;
+                case 5:
+                    movimientosValidos = "S,O";
+                    break;
+                default:
+                    movimientosValidos = "";
             }
 
             System.out.print("¿A dónde quieres ir? [" + movimientosValidos + "] > ");
@@ -59,22 +72,84 @@ public class NotitaAmorGPT {
                 // Cálculo de nueva posición y nombre de la dirección con Switches anidados
                 switch (posicionAlumno) {
                     case 0:
-                        switch (direccionChar) { case 'N': nuevaPosicion = 3; nombreDireccion = "Norte"; break; case 'E': nuevaPosicion = 1; nombreDireccion = "Este"; break; }
+                        switch (direccionChar) {
+                            case 'N':
+                                nuevaPosicion = 3;
+                                nombreDireccion = "Norte";
+                                break;
+                            case 'E':
+                                nuevaPosicion = 1;
+                                nombreDireccion = "Este";
+                                break;
+                        }
                         break;
                     case 1:
-                        switch (direccionChar) { case 'N': nuevaPosicion = 4; nombreDireccion = "Norte"; break; case 'E': nuevaPosicion = 2; nombreDireccion = "Este"; break; case 'O': nuevaPosicion = 0; nombreDireccion = "Oeste"; break; }
+                        switch (direccionChar) {
+                            case 'N':
+                                nuevaPosicion = 4;
+                                nombreDireccion = "Norte";
+                                break;
+                            case 'E':
+                                nuevaPosicion = 2;
+                                nombreDireccion = "Este";
+                                break;
+                            case 'O':
+                                nuevaPosicion = 0;
+                                nombreDireccion = "Oeste";
+                                break;
+                        }
                         break;
                     case 2:
-                        switch (direccionChar) { case 'N': nuevaPosicion = 5; nombreDireccion = "Norte"; break; case 'O': nuevaPosicion = 1; nombreDireccion = "Oeste"; break; }
+                        switch (direccionChar) {
+                            case 'N':
+                                nuevaPosicion = 5;
+                                nombreDireccion = "Norte";
+                                break;
+                            case 'O':
+                                nuevaPosicion = 1;
+                                nombreDireccion = "Oeste";
+                                break;
+                        }
                         break;
                     case 3:
-                        switch (direccionChar) { case 'S': nuevaPosicion = 0; nombreDireccion = "Sur"; break; case 'E': nuevaPosicion = 4; nombreDireccion = "Este"; break; }
+                        switch (direccionChar) {
+                            case 'S':
+                                nuevaPosicion = 0;
+                                nombreDireccion = "Sur";
+                                break;
+                            case 'E':
+                                nuevaPosicion = 4;
+                                nombreDireccion = "Este";
+                                break;
+                        }
                         break;
                     case 4:
-                        switch (direccionChar) { case 'S': nuevaPosicion = 1; nombreDireccion = "Sur"; break; case 'O': nuevaPosicion = 3; nombreDireccion = "Oeste"; break; case 'E': nuevaPosicion = 5; nombreDireccion = "Este"; break; }
+                        switch (direccionChar) {
+                            case 'S':
+                                nuevaPosicion = 1;
+                                nombreDireccion = "Sur";
+                                break;
+                            case 'O':
+                                nuevaPosicion = 3;
+                                nombreDireccion = "Oeste";
+                                break;
+                            case 'E':
+                                nuevaPosicion = 5;
+                                nombreDireccion = "Este";
+                                break;
+                        }
                         break;
                     case 5:
-                        switch (direccionChar) { case 'S': nuevaPosicion = 2; nombreDireccion = "Sur"; break; case 'O': nuevaPosicion = 4; nombreDireccion = "Oeste"; break; }
+                        switch (direccionChar) {
+                            case 'S':
+                                nuevaPosicion = 2;
+                                nombreDireccion = "Sur";
+                                break;
+                            case 'O':
+                                nuevaPosicion = 4;
+                                nombreDireccion = "Oeste";
+                                break;
+                        }
                         break;
                 }
 
